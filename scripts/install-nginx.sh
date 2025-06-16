80b5a643c1419069e0a5100bb964d3587bb827bd
cristian Algar <cristianfmorac@gmail.com>
2025-06-16 12:23:14 -0500

    feat:(semana 3): Instalación nginx

---
+++ scripts/install-nginx.sh
@@ -0,0 +1,30 @@
+echo "---Actualización de paquetes---"
+sudo apt-get update && sudo apt-get upgrade -y
+
+echo "---Instalación de Nginx---"
+sudo apt-get install nginx -y
+
+echo "---Habilitar Nginx para que se inicie al arrancar el sistema---"
+sudo systemctl enable nginx
+sudo systemctl enable nginx
+
+echo "---Iniciar Nginx---"
+sudo systemctl start nginx
+
+echo "---Habilitar el firewall para Nginx---"
+sudo ufw allow 'Nginx Full'
+
+echo "---Habilitar el firewall para SSH---"
+sudo ufw allow 22/tcp
+
+echo "---Habilitar el firewall---"
+sudo ufw enable
+
+echo "---Verificar el estado del firewall---"
+sudo ufw status
+
+echo "---Verificar el estado de Nginx---"
+sudo systemctl status nginx
+
+echo "---Verificar la instalación de Nginx---"
+curl -I http://localhost
